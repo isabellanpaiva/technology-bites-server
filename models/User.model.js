f0f5839f9ea92c5ecdf3d33969fdefd836b33502
Arian <arianraiesteban_20@hotmail.com>
2023-08-28 18:55:15 +0200

    version 1.3 auth routes and user routes

--- models/User.model.js
+++ models/User.model.js
@@ -1,72 +1,87 @@
-const { Schema, model } = require("mongoose")
+const { Schema, model } = require('mongoose')
+
+const bcrypt = require('bcryptjs')
 
 const userSchema = new Schema(
-  {
+	{
+		firstName: {
+			type: String,
+			required: [true, 'First name is required.'],
+		},
 
-    firstName: {
-      type: String,
-      required: [true, 'First name is required.']
-    },
+		lastName: {
+			type: String,
+			required: [true, 'First name is required.'],
+		},
 
-    lastName: {
-      type: String,
-      required: [true, 'First name is required.']
-    },
+		email: {
+			type: String,
+			required: [true, 'Email is required.'],
+			unique: [true, 'Email already exists'],
+			lowercase: true,
+			trim: true,
+		},
 
-    email: {
-      type: String,
-      required: [true, 'Email is required.'],
-      unique: true,
-      lowercase: true,
-      trim: true
-    },
+		password: {
+			type: String,
+			required: [true, 'Password is required.'],
+			minlength: [4, 'The password needs to be at least 4 characters'],
+		},
 
-    password: {
-      type: String,
-      required: [true, 'Password is required.']
-    },
+		avatar: {
+			type: String,
+			default: 'DEFAULTAVATAR',
+		},
 
-    avatar: {
-      type: String,
-      // default: 
-    },
+		jobPosition: {
+			type: String,
+		},
 
-    position: {
-      type: String,
-    },
+		description: {
+			type: String,
+			minlength: [20, 'Description must be 20 characters minimum'],
+		},
 
-    description: {
-      type: String,
-      minlength: [20, 'Description must be 20 characters minimum']
-    },
+		friends: [
+			{
+				type: Schema.Types.ObjectId,
+				ref: 'User',
+			},
+		],
 
-    friends: [{
-      type: Schema.Types.ObjectId,
-      ref: 'User'
-    }],
+		role: {
+			type: String,
+			enum: ['USER', 'ADMIN'],
+			default: 'USER',
+		},
 
-    role: {
-      type: String,
-      enum: ['USER', 'ADMIN'],
-      default: 'USER'
-    },
+		social: [
+			{
+				platform: {
+					type: String,
+					enum: ['LinkedIn', 'Github', 'Instagram', 'Facebook', 'X'],
+				},
+				link: {
+					type: String,
+				},
+			},
+		],
+	},
 
-    social: [{
-      platform: {
-        type: String,
-        enum: ['LinkedIn', 'Github'],
-      },
-      link: {
-        type: String,
-      }
-    }],
-  },
-
-  {
-    timestamps: true
-  }
+	{
+		timestamps: true,
+	}
 )
 
-const User = model("User", userSchema)
+userSchema.pre('save', function (next) {
+	const saltRounds = 10
+	const salt = bcrypt.genSaltSync(saltRounds)
+	const hashedPassword = bcrypt.hashSync(this.password, salt)
+	this.password = hashedPassword
+
+	next()
+})
+
+const User = model('User', userSchema)
 
 module.exports = User
