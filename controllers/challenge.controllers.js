54c7b21fb82782feb86c17f7ea6d8bb61fc74966
Arian <arianraiesteban_20@hotmail.com>
2023-09-01 10:23:30 +0200

    version 2.3 challenges

--- controllers/challenge.controllers.js
+++ controllers/challenge.controllers.js
@@ -1,22 +1,23 @@
-const Bite = require('../models/Bite.model')
-const bitesJson = require('../consts/bites.json')
+const Challenge = require('../models/Challenge.model')
+const challengeJson = require('../consts/challenges.json')
 
 const createOne = (req, res, next) => {
-	const biteInfo = ({ category, definition } = req.body)
-	Bite.create(biteInfo)
+	const challengeInfo = ({ category } = req.body)
+
+	Challenge.create(challengeInfo)
 		.then(() => res.sendStatus(200))
 		.catch(err => next(err))
 }
 
 const createMany = (req, res, next) => {
-	Bite.insertMany(bitesJson)
+	Challenge.insertMany(challengeJson)
 		.then(() => res.sendStatus(200))
 		.catch(err => next(err))
 }
 
 const getOneRandom = (req, res, next) => {
-	Bite.aggregate([{ $sample: { size: 1 } }])
-		.project({ category: 1, definition: 1 })
+	Challenge.aggregate([{ $sample: { size: 1 } }])
+		.project({ category: 1 })
 		.then(response => res.json(response))
 		.catch(err => next(err))
 }
--- routes/challenge.routes.js
+++ routes/challenge.routes.js
@@ -1,3 +1,9 @@
 const router = require('express').Router()
+const { createOne, createMany, getOneRandom } = require('../controllers/challenge.controllers')
+const { verifyauthBD } = require('../middleware/verifyAuthBD.middleware')
+
+router.post('/createOne', verifyauthBD, createOne)
+router.post('/createMany', verifyauthBD, createMany)
+router.get('/getOneRandom', getOneRandom)
 
 module.exports = router
